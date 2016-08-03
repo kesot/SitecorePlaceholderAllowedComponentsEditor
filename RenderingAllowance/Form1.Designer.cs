d3512b5c68ef2fa82ea572e9c9991a8afa1c2c08
kesot <ershov.k@outlook.com>
2016-08-03 08:28:42 +0200

    init

---
+++ RenderingAllowance/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿using MultiSelectTreeview = RenderingAllowance.MultiSelectTreeview;
+
+namespace RenderingAllowance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new RenderingAllowance.MultiSelectTreeview();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectScprojToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.treeView1, 2);
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Margin = new System.Windows.Forms.Padding(0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedNodes = ((System.Collections.Generic.List<System.Windows.Forms.TreeNode>)(resources.GetObject("treeView1.SelectedNodes")));
+            this.treeView1.Size = new System.Drawing.Size(436, 400);
+            this.treeView1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(436, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectScprojToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // selectScprojToolStripMenuItem
+            // 
+            this.selectScprojToolStripMenuItem.Name = "selectScprojToolStripMenuItem";
+            this.selectScprojToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.selectScprojToolStripMenuItem.Text = "Select scproj";
+            this.selectScprojToolStripMenuItem.Click += new System.EventHandler(this.selectScprojToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(3, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "AddIfNotExists";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(133, 407);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(288, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 130F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.treeView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(436, 430);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 454);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MultiSelectTreeview treeView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectScprojToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RenderingAllowance/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace RenderingAllowance
+{
+    public partial class Form1 : Form
+    {
+        private string scProjPath;
+        private List<TreeNode> selectedNodes;
+        string ProjectDirectory => Path.GetDirectoryName(scProjPath);
+
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "Sitecore project|*.scproj";
+            openFileDialog1.FileOk += (sender, args) =>
+            {
+                treeView1.Nodes.Clear();
+                treeView1.SelectedNodes.Clear();
+                scProjPath = openFileDialog1.FileName;
+                FindAllPlaceholders();
+                treeView1.ExpandAll();
+            };
+        }
+
+        private void selectScprojToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void FindAllPlaceholders()
+        {
+            if (scProjPath == null)
+                throw new InvalidOperationException($"{nameof(scProjPath)} == null");
+            var doc = new XmlDocument();
+            doc.Load(scProjPath);
+            var sitecoreXElements = doc.GetElementsByTagName("SitecoreItem");
+            for (var i = 0; i < sitecoreXElements.Count; i++)
+            {
+                XmlNode sitecoreXElement = sitecoreXElements[i];
+                var itemPath = HttpUtility.UrlDecode(sitecoreXElement.Attributes.GetNamedItem("Include").Value);
+                var itemFileText = File.ReadAllText(Path.Combine(ProjectDirectory, itemPath));
+                if (itemFileText.Contains("templatekey: Placeholder"))
+                {
+                    CreateNodeAndParents(itemPath);
+                }
+            }
+        }
+
+        private void CreateNodeAndParents(string itemPath)
+        {
+            var itemsPaths = itemPath.Split('\\');
+            var rootNode = treeView1.Nodes[itemsPaths[0]] ?? treeView1.Nodes.Add(itemsPaths[0], itemsPaths[0]);
+            var pointer = rootNode;
+            foreach (var key in itemsPaths.Skip(1).Take(itemsPaths.Length))
+            {
+                var node = pointer.Nodes[key] ?? pointer.Nodes.Add(key, key);
+                pointer = node;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (scProjPath == null)
+            {
+                MessageBox.Show("Select sitecore project");
+                return;
+            }
+            var guidString = textBox1.Text;
+            Guid guid;
+            Guid.TryParse(guidString.Replace("{", "").Replace("}", ""), out guid);
+            if (guid == Guid.Empty)
+            {
+                MessageBox.Show("incorrect guid");
+                return;
+            }
+            UpdateAllowanceForSelected(guid);
+        }
+
+        private void UpdateAllowanceForSelected(Guid guid)
+        {
+            var filesToSave = new Dictionary<string, IEnumerable<string>>();
+
+            foreach (var selectedNode in treeView1.SelectedNodes)
+            {
+                var nodePath = selectedNode.FullPath;
+                var nodeFullPath = Path.Combine(ProjectDirectory, nodePath);
+                var itemFileLines = File.ReadAllLines(nodeFullPath);
+                var allowanceKeyIndex = GetFieldIndex(nodeFullPath, "key: allowed controls", itemFileLines);
+
+                var contentLengthIndex = allowanceKeyIndex + 1;
+                var contentIndex = contentLengthIndex + 2;
+
+                // todo order by Name
+                var allowedControlGuids =
+                    new HashSet<Guid>(itemFileLines[contentIndex].Replace("{", "").Replace("}", "")
+                        .Split(new [] {'|'}, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(s => Guid.Parse(s)));
+
+                allowedControlGuids.Add(guid);
+
+                itemFileLines[contentIndex] = string.Join("|",
+                    allowedControlGuids.Select(g => g.ToString("B").ToUpper()));
+
+                itemFileLines[contentLengthIndex] = $"content-length: {itemFileLines[contentIndex].Length}";
+                filesToSave.Add(nodeFullPath, itemFileLines);
+
+                var updatedAtIndex = GetFieldIndex(nodeFullPath, "key: __updated", itemFileLines, 3);
+                var updatedAtString = DateTime.UtcNow.ToString("yyyyMMddTHHmmssZ");
+                itemFileLines[updatedAtIndex] = updatedAtString;
+
+                var newRevisionGuid = Guid.NewGuid().ToString("D");
+                var revisionIndex = GetFieldIndex(nodeFullPath, "key: __revision", itemFileLines, 3);
+                itemFileLines[revisionIndex] = newRevisionGuid;
+
+                var versionIndex = GetFieldIndex(nodeFullPath, "----version----", itemFileLines, 3);
+                itemFileLines[versionIndex] = $"revision: {newRevisionGuid}";
+            }
+
+            foreach (var fileToSave in filesToSave)
+            {
+                File.WriteAllLines(fileToSave.Key, fileToSave.Value);
+            }
+
+            MessageBox.Show("Done");
+        }
+
+        private int GetFieldIndex(string nodeFullPath, string fieldName, string[] lines, int offset = 0)
+        {
+            var index = lines
+                    .Select((l, i) => new { Line = l, Index = i })
+                    .FirstOrDefault(l => l.Line == fieldName)
+                    ?.Index + offset;
+
+            if (!index.HasValue)
+            {
+                throw new InvalidOperationException($"Placeholder doesn't have field: ${fieldName}. No changes saved! Path: {nodeFullPath}");
+            }
+            return index.Value;
+        }
+    }
+}
